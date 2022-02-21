4067fea240b8e206ea7b713d910dc204b945832a
KukuJerung <khairulfilhan94@gmail.com>
2022-02-22 00:10:26 +0800

    config - nearby places added

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,4 @@
 export const gmapPlacesAutoCompleteUrl = 'https://maps.googleapis.com/maps/api/place/autocomplete/json?'
+export const gmapPlacesNearbyUrl = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
 export const gmapGeocodingUrl = 'https://maps.googleapis.com/maps/api/geocode/json?'
 export const proxyUrl = 'https://cors-anywhere.herokuapp.com'
