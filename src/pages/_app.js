7f0bef2424fc7ced6897536ddf0ef99ab6c6a769
KukuJerung <khairulfilhan94@gmail.com>
2022-02-21 16:17:30 +0800

    pages - app/document nextjs

---
+++ src/pages/_app.js
@@ -0,0 +1,33 @@
+
+import React from 'react'
+import { Provider } from 'react-redux'
+import PropTypes from 'prop-types'
+import { useStore } from '../redux/store'
+import { StyledEngineProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import { CacheProvider } from '@emotion/react'
+import createEmotionCache from '../utils/emotion-cache'
+
+const clientSideEmotionCache = createEmotionCache()
+
+export default function App ({ Component, pageProps, emotionCache }) {
+  const store = useStore(pageProps.initialReduxState)
+  emotionCache = clientSideEmotionCache
+
+  return (
+    <Provider store={store}>
+          <CacheProvider value={emotionCache}>
+        <StyledEngineProvider injectFirst>
+          <CssBaseline />
+      <Component {...pageProps} />
+      </StyledEngineProvider>
+      </CacheProvider>
+    </Provider>
+  )
+}
+
+App.propTypes = {
+  pageProps: PropTypes.object.isRequired,
+  Component: PropTypes.elementType.isRequired,
+  emotionCache: PropTypes.object
+}
